1d2e969c686ace39722e2167cd412345f884d413
jirathip-k <jkun0008@student.monash.edu>
2023-10-25 11:03:02 +1100

    better path

--- mate-frontend/src/app/index.tsx
+++ mate-frontend/src/app/index.tsx
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
+import Hello from "@/components/Hello";
 
 export default function Page() {
   return (
     <View className="flex bg-red-400 items-center justify-center h-screen">
       <View className="max-w-2xl p-8 bg-white rounded-lg shadow">
-        <Text className="text-6xl font-bold">Hello World</Text>
+        <Hello />
         <Text className="text-3xl text-gray-700">
           This is the first page of your app.
         </Text>
---
+++ mate-frontend/src/components/Hello.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default () => {
+  return <Text className="text-6xl font-bold">Hello World</Text>;
+};
